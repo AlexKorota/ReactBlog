09c83d2b4faeaa02e28393fc2b82c457a70c836c
north <norther48@list.ru>
2020-01-14 17:23:34 +0300

    blog flux files

--- ReactBlog/App/index.jsx
+++ ReactBlog/App/index.jsx
@@ -1,8 +1,20 @@
 ﻿import React from 'react'
 import { render } from 'react-dom'
-import App from './containers/app.jsx'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import App from './app.jsx'
+import rootReducer from './rootReducer.jsx'
+
+function configureStore(initialState) {
+    return createStore(rootReducer, initialState, applyMiddleware(thunk))
+}
+
+const store = configureStore()
 
 render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('content')
 )
